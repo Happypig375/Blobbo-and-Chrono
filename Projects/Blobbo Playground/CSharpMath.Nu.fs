95014c11f2d83db82eb91beec012b8d22e5439d7
Hadrian Tang <hadrianwttang@outlook.com>
2026-01-28 03:09:20 +0800

    MathFacet done

--- Projects/Blobbo Playground/CSharpMath.Nu.fs
+++ Projects/Blobbo Playground/CSharpMath.Nu.fs
@@ -11,7 +11,7 @@ module [<AutoOpen>] Helpers =
     let toDrawingColor (color: Color) =
         Drawing.Color.FromArgb (int color.A8, int color.R8, int color.G8, int color.B8)
 
-type NuCanvas (transform : Transform, world : Nu.World) =
+type NuCanvas (transform : Transform inref, world : Nu.World) =
     let mutable transform = transform
     member _.World = world
     member _.Transform = transform
@@ -19,6 +19,7 @@ type NuCanvas (transform : Transform, world : Nu.World) =
     member val CurrentColor = Nullable<Drawing.Color> () with get, set
     member val DefaultColor = Drawing.Color.Black with get, set
     member val CurrentStyle = CSharpMath.Rendering.FrontEnd.PaintStyle.Fill with get, set
+    member this.Scale (sx, sy) = transform.Scale <- v3 (transform.Scale.X * sx) (transform.Scale.Y * sy) 1f
     interface CSharpMath.Rendering.FrontEnd.ICanvas with
         member this.Width = transform.Size.X * transform.Scale.X
         member this.Height = transform.Size.Y * transform.Scale.Y
@@ -67,7 +68,7 @@ type NuCanvas (transform : Transform, world : Nu.World) =
         member this.Save () = this.TransformStack.Push transform
         member this.Restore () = transform <- this.TransformStack.Pop ()
         member this.Translate (dx, dy) = transform.Position <- transform.Position + (v3 dx dy 0f * transform.Scale * transform.Size).Transform transform.Rotation
-        member this.Scale (sx, sy) = () //transform.Scale <- v3 (transform.Scale.X * sx) (transform.Scale.Y * sy) 1f
+        member this.Scale (sx, sy) = this.Scale (sx, sy)
         member this.StartNewPath () = new NuPath (this)
 
 and NuPath (owner : NuCanvas) =
@@ -99,29 +100,51 @@ type MathPainter () =
 
 namespace BlobboPlayground
 open Nu
+open CSharpMath.Nu
+open CSharpMath.Rendering.FrontEnd
 open System.Numerics
 module [<AutoOpen>] MathFacetExtensions =
     type Entity with
         member this.GetLaTeX world : string = this.Get (nameof this.LaTeX) world
         member this.SetLaTeX (value : string) world = this.Set (nameof this.LaTeX) value world
         member this.LaTeX = lens (nameof this.LaTeX) this this.GetLaTeX this.SetLaTeX
+        member this.GetMathFontSize world : single = this.Get (nameof this.MathFontSize) world
+        member this.SetMathFontSize (value : single) world = this.Set (nameof this.MathFontSize) value world
+        member this.MathFontSize = lens (nameof this.MathFontSize) this this.GetMathFontSize this.SetMathFontSize
+        member this.GetMathPainter world : MathPainter = this.Get (nameof this.MathPainter) world
+        member this.MathPainter = lensReadOnly (nameof this.MathPainter) this this.GetMathPainter
 
 type MathFacet () =
     inherit Facet (false, false, false)
 
     static member Properties =
         [define Entity.LaTeX ""
-         define Entity.FontSizing None
-         define Entity.Color Color.White]
+         define Entity.MathFontSize 12f
+         define Entity.Color Color.White
+         nonPersistent Entity.MathPainter (MathPainter ())]
+
+    override this.Register (entity, world) =
+        World.sense (fun event world ->
+            let painter = event.Subscriber.GetMathPainter world
+            painter.LaTeX <- (event.Data : ChangeData).Value :?> string
+            let size = painter.Measure 0f
+            event.Subscriber.SetSize (v3 size.Width size.Height 0f) world
+            Cascade) entity.LaTeX.ChangeEvent entity (nameof MathFacet) world
+        World.sense (fun event world ->
+            let painter = event.Subscriber.GetMathPainter world
+            painter.FontSize <- (event.Data : ChangeData).Value :?> single
+            let size = painter.Measure 0f
+            event.Subscriber.SetSize (v3 size.Width size.Height 0f) world
+            Cascade) entity.MathFontSize.ChangeEvent entity (nameof MathFacet) world
+        World.sense (fun event world ->
+            (event.Subscriber.GetMathPainter world).TextColor <- (event.Data : ChangeData).Value :?> Color
+            Cascade) entity.Color.ChangeEvent entity (nameof MathFacet) world
 
     override this.Render (_, entity, world) =
-        let canvas = CSharpMath.Nu.NuCanvas (entity.GetTransform world, world)
-        let painter = CSharpMath.Nu.MathPainter (LaTeX = entity.GetLaTeX world, TextColor = entity.GetColor world)
-        match entity.GetFontSizing world with Some f -> painter.FontSize <- single f | None -> ()
-        painter.Draw (canvas, -0.5f, 0f)
-
-    override this.GetAttributesInferred (entity, world) =
-        let painter = CSharpMath.Nu.MathPainter (LaTeX = entity.GetLaTeX world)
-        match entity.GetFontSizing world with Some f -> painter.FontSize <- single f | None -> ()
-        let size = painter.Measure 0f
-        AttributesInferred.important (v3 size.Width size.Height 0f) v3Zero
+        let painter = entity.GetMathPainter world
+        let height = painter.Display.Ascent + painter.Display.Descent
+        if painter.Display.Width > 0f && height > 0f then
+            let transform = entity.GetTransform world
+            let canvas = NuCanvas (&transform, world)
+            canvas.Scale (1f / painter.Display.Width, -1f / height)
+            painter.Draw (canvas, painter.Display.Width * -0.5f, (painter.Display.Ascent - painter.Display.Descent) * 0.5f)
